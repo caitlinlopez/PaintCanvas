07e526967d31f4f73f9c7889427c7eb34bcfcb6c
caitlin <caitlinlopez@users.noreply.github.com>
2016-12-12 21:02:37 -0800

    Steps 1-7.

--- project7.js
+++ project7.js
@@ -1,6 +1,8 @@
 var screen = document.getElementById("screen")
 var namespace = "http://www.w3.org/2000/svg"
 
+var pen = false;
+
 // utility function
 function transformPoint(event) {
   var pt = screen.createSVGPoint()
@@ -12,11 +14,52 @@ function transformPoint(event) {
 
 // Step 2: drawSquare and drawCircle functions
 function drawSquare(x, y, size, color) {
-  // square drawing code here
+  var square = document.createElementNS(namespace, "rect")
+ square.setAttribute("x", x)
+ square.setAttribute("y", y)
+ square.setAttribute("width", size)
+ square.setAttribute("height", size)
+ square.setAttribute("fill", color)
+ screen.appendChild(square)
+}
+
+function drawCircle(x, y, size, color) {
+  var circle = document.createElementNS(namespace, "circle")
+ circle.setAttribute("cx", x)
+ circle.setAttribute("cy", y)
+ circle.setAttribute("r", size)
+ circle.setAttribute("fill", color)
+ screen.appendChild(circle)
 }
 
 // Step 3: Event listeners
 document.addEventListener("mousedown", function(e) {
-  // what do you want to do when the user presses down
-  // on the mouse button?
+  pen = true;
 })
+document.addEventListener("mouseup", function(e) {
+  pen = false;
+})
+
+document.addEventListener("mousemove", function(e) {
+ var color = document.getElementById("colorSelect").value
+ var shape = document.getElementById("shapeSelect").value
+ var size = document.getElementById("sizeSelect").value
+
+   if (pen){
+     var pt = transformPoint(e, screen)
+
+     if(color == "red" && shape == "circle" && size == "10"){
+      drawCircle(pt.x, pt.y, 10, "red")
+     }
+      else if(color == "blue" && shape == "circle" && size == "10"){
+       drawCircle(pt.x, pt.y, 10, "blue")
+     }
+     else if(color == "green" && shape == "circle" && size == "10"){
+      drawCircle(pt.x, pt.y, 10, "green")
+    }
+    else if(color == "yellow" && shape == "circle" && size == "10"){
+     drawCircle(pt.x, pt.y, 10, "yellow")
+   }
+    }
+
+ })
